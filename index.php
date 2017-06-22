b1e29c862e713b30aae41c32510265f3f340b9a2
Yuchi Miki <choose_the_life@hotmail.com>
2017-06-22 08:36:20 +0000

    add　crlf nya

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ foreach ($events as $event) {
     error_log('Non text message has come');
     continue;
   }
-  $bot->replyText($event->getReplyToken(), $event->getText() . "にゃ");
+  $bot->replyText($event->getReplyToken(), $event->getText() . "\nにゃ");
 }
 
  ?>
