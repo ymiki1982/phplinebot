1bda5894784a0fe6b30d06e4a4b6dd44c53d9976
Yuchi Miki <choose_the_life@hotmail.com>
2017-06-26 08:18:00 +0000

    edit remaindmassage

--- remindmessage.php
+++ remindmessage.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-$id = getenv('SEND_ID')
+$id = getenv('SEND_ID');
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("時間にゃ");
 $response = $bot->pushMessage($id, $textMessageBuilder);
