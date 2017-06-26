df2b6a404c36e7531a1194bd53c575deb981a0e3
Yuchi Miki <choose_the_life@hotmail.com>
2017-06-26 08:01:30 +0000

    send push message

---
+++ remindmessage.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$id = getenv('SEND_ID')
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("時間にゃ");
+$response = $bot->pushMessage($id, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+ ?>
