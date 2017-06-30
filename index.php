f3228ad65380853247fb9c5737af469af1906db1
Yuchi Miki <choose_the_life@hotmail.com>
2017-06-30 00:43:40 +0000

    edit replay message

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ foreach ($events as $event) {
     error_log('Non text message has come');
     continue;
   }
-  $bot->replyText($event->getReplyToken(), $event->getText() . $event->getUserId() . "\nにゃ");
+  $bot->replyText($event->getReplyToken(), $event->getText() . "\nにゃ");
 
 }
 
