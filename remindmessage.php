4aca69b5bbf02bb27b5581b63701b9557f3c0a9f
Yuchi Miki <choose_the_life@hotmail.com>
2017-07-10 04:43:44 +0000

    remind message change

--- remindmessage.php
+++ remindmessage.php
@@ -6,7 +6,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 $id = getenv('SEND_ID');
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("鼻プシュタイムにゃ!!");
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("おくすり(鼻プシュ)の時間です。");
 $response = $bot->pushMessage($id, $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
