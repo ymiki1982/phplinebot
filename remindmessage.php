8882c26c1fe91500f16113a92d35860ba8a32469
ymiki1982 <choose_the_life@hotmail.com>
2020-07-20 00:08:51 +0900

    add tabletsremaind

--- remindmessage.php
+++ remindmessage.php
@@ -6,24 +6,31 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 $id = getenv('SEND_ID');
 
-$arrayRemind = array(
-  "drugs飲むにゃー",
-  "this ia drug time miaw!",
-  "お薬の時間だよー",
-  "いま おくすり のむ とき にゃ",
-  "はーい、おくすりの時間ですよー"
-);
-
-$indexArray = rand(0,count($arrayRemind)-1);
-
 date_default_timezone_set('Asia/Tokyo');
-if (date('a')=='am'){
-    $strMs = '鼻シュータイムにゃー!!';
-}else{
-    $strMs = $arrayRemind[$indexArray];
-}
 
-$textRemindMs = $strMs;
+$today = date("Y/m/d");
+if(strtotime("2020/7/19") <= strtotime($today) && strtotime($today) <= strtotime("2020/7/21")){
+  $textRemindMs = "1錠のむにゃ";
+}else if(strtotime("2020/7/22") <= strtotime($today) && strtotime($today) <= strtotime("2020/7/24")){
+  $textRemindMs = "2錠のむにゃ";
+}else if(strtotime("2020/7/25") <= strtotime($today) && strtotime($today) <= strtotime("2020/7/27")){
+  $textRemindMs = "3錠のむにゃ";
+}else if(date("H") = "22"){
+
+    $arrayRemind = array(
+		  "drugs飲むにゃー",
+		  "this ia drug time miaw!",
+		  "お薬の時間だよー",
+		  "いま おくすり のむ とき にゃ",
+		  "はーい、おくすりの時間ですよー"
+    );
+    
+    $indexArray = rand(0,count($arrayRemind)-1);
+    $textRemindMs = $arrayRemind[$indexArray];   
+
+}else{
+    exit;
+}
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($textRemindMs);
 $response = $bot->pushMessage($id, $textMessageBuilder);
